e47aa2d8ba398ac2f81e4aa555ca88776e4033c2
Michael Sondergaard <msond@cs.au.dk>
2015-07-08 14:10:52 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Forged Alliance Forever Tools',
+    version='0.1',
+    packages=['api', 'lua', 'mods'],
+    url='http://www.faforever.com',
+    license='GPLv3',
+    author='Michael Sondergaard',
+    author_email='sheeo@sheeo.dk',
+    description='faftools project'
+)
