405b5fba07e1dc07f1403a19c7496db5e040a56a
Michael Sondergaard <msond@cs.au.dk>
2015-07-14 11:12:36 +0200

    Make update_version runnable as module
    Add documentation for it.

---
+++ faftools/fa/update_version.py
@@ -0,0 +1,27 @@
+"""
+Updates the version in the binary executable of the Forged Alliance game. Will write a new ForgedAlliance.version.exe file.
+
+Usage:
+   update_version <version> [--file=<file>]
+
+Options:
+   --file=<file>   The binary file to update [default: ForgedAlliance.exe]
+"""
+import struct
+import shutil
+from docopt import docopt
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    file, version = arguments.get('--file'), arguments.get('<version>')
+    shutil.copyfile(file, "ForgedAlliance.%s.exe" % version)
+    exe = open("ForgedAlliance.exe", 'rb').read()
+    addr = [0xd3d3f, 0x47612c, 0x476665]
+    f = open("ForgedAlliance.%s.exe" % version, 'rb+')
+
+    for a in addr:
+        v = struct.pack("<L", version)
+        f.seek(a+1, 0)
+        f.write(v)
+    f.close()
+    print("Saved ForgedAlliance.%s.exe".format(version))
