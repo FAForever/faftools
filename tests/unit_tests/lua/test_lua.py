5930d7dac68accf681fd5c99ac4e0a00dcadbd2b
Sheeo <sheeo@faforever.com>
2015-08-28 17:59:29 +0200

    Add missing lua emission tests

--- tests/unit_tests/lua/test_lua.py
+++ tests/unit_tests/lua/test_lua.py
@@ -8,10 +8,23 @@ def test_emits_number():
     assert lua.to_lua(5) == '5'
 
 
+def test_emits_empty_dictionary():
+    assert lua.to_lua({}) == '{}'
+
+
 def test_emits_dictionary():
     expected = {'some_key': 'some-value', 'other_key': 42}
     for k, v in lua.from_lua('result = ' + lua.to_lua(expected))['result'].items():
         assert expected[k] == v
+
+def test_emits_empty_list():
+    data = []
+    assert data == list(lua.from_lua('result = ' + lua.to_lua(data))['result'].values())
+
+
+def test_emits_list():
+    data = ['a', 'b', 'c']
+    assert data == list(lua.from_lua('result = ' + lua.to_lua(data))['result'].values())
 
 
 def test_emits_variable_assignment():
