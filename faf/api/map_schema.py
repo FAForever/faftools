b678aad5da0cc8fe134c3409858f2d93083aec17
Sheeo <sheeo@faforever.com>
2015-11-20 02:14:24 +0100

    Reorder map schema properties for readability

--- faf/api/map_schema.py
+++ faf/api/map_schema.py
@@ -8,17 +8,20 @@ class MapSchema(Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
     description = fields.String(required=True)
+    version = fields.String(required=True)
+    filename = fields.String(required=True)
+
     map_type = fields.String()
     max_players = fields.Integer()
     battle_type = fields.String()
     map_size_x = fields.Float()
     map_size_y = fields.Float()
-    version = fields.String(required=True)
-    filename = fields.String(required=True)
-    downloads = fields.Integer()
-    num_draws = fields.Integer()
-    times_played = fields.Integer()
-    rating = fields.Float()
+
+    # Read only fields
+    downloads = fields.Integer(dump_only=True)
+    num_draws = fields.Integer(dump_only=True)
+    times_played = fields.Integer(dump_only=True)
+    rating = fields.Float(dump_only=True)
 
     class Meta:
         type_ = 'maps'
