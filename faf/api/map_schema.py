929d684edd55fdef8a36a40df6c3f6823f912ebc
Michel Jung <michel.jung89@gmail.com>
2016-06-25 23:37:19 +0200

    Add create_time and update_time

--- faf/api/map_schema.py
+++ faf/api/map_schema.py
@@ -27,6 +27,8 @@ class MapSchema(Schema):
     thumbnail_url_small = fields.Url(dump_only=True)
     thumbnail_url_large = fields.Url(dump_only=True)
     technical_name = fields.String(dump_only=True)
+    create_time = fields.DateTime(dump_only=True)
+    update_time = fields.DateTime(dump_only=True)
 
     class Meta:
         type_ = 'map'
