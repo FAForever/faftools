8a6b12129a583f8c4e5223f59a40a18e8a94c6cf
Michel Jung <michel.jung89@gmail.com>
2016-08-21 02:47:12 +0200

    Adding files to git helps Yes I know I should've worked on a branch, sorry

---
+++ tests/data/maps/theta passage 5.v0001/theta passage 5_scenario.lua
@@ -0,0 +1,31 @@
+version = 3
+ScenarioInfo = {
+    name = 'Theta Passage 5',
+    description = '',
+    type = 'skirmish',
+    starts = true,
+    preview = '',
+    size = {256, 256},
+    map_version = 1,
+    map = '/maps/theta passage 5.v0001/theta passage 5.scmap',
+    save = '/maps/theta passage 5.v0001/theta passage 5_save.lua',
+    script = '/maps/theta passage 5.v0001/theta passage 5_script.lua',
+	description="Balanced Version of Theta Passage 2. Now the Reclaim is equal at the top/bottom and at the left/right side at the middle. Also there are no longer any stones hidden inside a Hill.",
+    norushradius = 50.000000,
+    norushoffsetX_ARMY_1 = 15.000000,
+    norushoffsetY_ARMY_1 = -15.000000,
+    norushoffsetX_ARMY_2 = -15.000000,
+    norushoffsetY_ARMY_2 = 15.000000,
+    norushoffsetX_ARMY_3 = 0.000000,
+    norushoffsetY_ARMY_3 = 0.000000,
+    norushoffsetX_ARMY_4 = 0.000000,
+    norushoffsetY_ARMY_4 = 0.000000,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                { name = 'FFA', armies = {'ARMY_1','ARMY_2','ARMY_3','ARMY_4',} },
+            },
+            customprops = {
+            },
+        },
+    }}
