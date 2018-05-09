504ded18ce13b3aa9a533a3dcaa795b51823c8ba
Brutus5000 <Brutus5000@gmx.net>
2018-05-09 23:31:02 +0200

    Explicitly give dependencies, remove pip internals

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-from pip.req import parse_requirements
-
 setup(
     name='faftools',
     version='0.1',
@@ -13,7 +11,14 @@ setup(
     author_email='sheeo@faforever.com',
     description='faftools project',
     requires=['lupa'],
-    install_requires=[str(r.req) for r in parse_requirements('requirements.txt', session=False)
-                                 if r.req is not None],
+    install_requires=['docopt',
+                      'lupa',
+                      'marshmallow==2.13.1',
+                      'marshmallow-jsonapi',
+                      'pathlib',
+                      'requests',
+                      'pytest-cov',
+                      'pytest-mock',
+                      'werkzeug'],
     include_package_data=True
 )
